3854deea3c214dd2f0d7458269aff904beabe846
Ryan Lim <rzlim08@gmail.com>
2025-04-24 14:39:22 -0700

    fix overlay-root

--- start_webapp.sh
+++ start_webapp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to start the web application for the Copick project.
-uv run copick_server/server.py serve --dataset-ids 10440 --dataset-ids 10441 --dataset-ids 10445 --dataset-ids 10446 --host 0.0.0.0 --port 8012 --overlay_root /mnt/copick/overlay
+uv run copick_server/server.py serve --dataset-ids 10440 --dataset-ids 10441 --dataset-ids 10445 --dataset-ids 10446 --host 0.0.0.0 --port 8012 --overlay-root /mnt/copick/overlay
